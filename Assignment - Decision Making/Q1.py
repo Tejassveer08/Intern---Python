8e8aadb8b02e2dbf3aac86d27e3de0bb54f6827a
Tejassveer Vasant <147965837+Tejassveer08@users.noreply.github.com>
2025-06-24 15:36:01 +0530

    Update Q1.py

--- Assignment - Decision Making/Q1.py
+++ Assignment - Decision Making/Q1.py
@@ -1,1 +1,9 @@
+cp = float(input("Enter the cost price of the product: "))
+sp = float(input("Enter the selling price of the product: "))
 
+if sp > cp:
+    print("Profit", sp - cp)
+elif sp < cp:
+    print("Loss", cp - sp)
+else:
+    print("No Profit No Loss")
